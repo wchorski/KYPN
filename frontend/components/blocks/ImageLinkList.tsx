c17861866dd51b3d8b2d75574890587910e81482
wchorski <wchorski@gmail.com>
2023-06-14 09:49:01 -0500

    image links working

--- frontend/components/blocks/ImageLinkList.tsx
+++ frontend/components/blocks/ImageLinkList.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import { ReactNode } from "react"
+import { LuExternalLink } from "react-icons/lu";
 import styled from "styled-components"
 
 type InfoCard = {
@@ -33,21 +34,40 @@ function Card({item}:{item:InfoCard}){
 
   return (
     <StyledCard>
-      <header>
-        <figure 
-        // todo maybe do a blur?
-          // style={{backgroundImage: `url(${item.imageSrc})`}}
-        >
-          <img src={item.imageSrc}/>
-        </figure>
-        <h3>{item.header}</h3>
-      </header>
 
-      <div className="content">
+      {item.header && <h6>{item.header}</h6> }
+      
+
+      {item.buttonLink ? (
+        <Link href={item.buttonLink} target="#" className="image-cont">
+          <figure 
+          // todo maybe do a blur?
+            // style={{backgroundImage: `url(${item.imageSrc})`}}
+          >
+            <img src={item.imageSrc}/>
+          </figure>
+        </Link>
+      ) : (
+        <div className="image-cont">
+          <figure 
+          // todo maybe do a blur?
+            // style={{backgroundImage: `url(${item.imageSrc})`}}
+          >
+            <img src={item.imageSrc}/>
+          </figure>
+        </div>
+      )}
+
+      {/* <div className="content">
         {item.content}
-      </div>
+      </div> */}
+      {item.buttonLink && (
+        <Link href={item.buttonLink} target="#" className="button">
+          {item.buttonLabel}
+          <LuExternalLink />
+        </Link>
+      )}
 
-      <Link href={item.buttonLink} className="button">{item.buttonLabel}</Link>
     </StyledCard>
   )
 }
@@ -59,26 +79,43 @@ const StyledList = styled.ul`
   justify-content: center;
   align-items: stretch;
   gap: 1rem;
+  list-style: none;
   padding: 0 1rem;
 
   li{
     min-width: 16rem;
+    margin-bottom: 2rem;
     flex: 1;
   }
 `
 
-const StyledCard = styled.article`
+const StyledCard = styled.div`
   padding: 0;
   padding-bottom: 1em;
-  border: solid 1px var(--c-desaturated);
+  
+  /* border: solid 1px var(--c-desaturated); */
   height: 100%;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
   transition: all .3s;
-  overflow: hidden;
+  /* overflow: hidden; */
+  position: relative;
 
-  h3{
+  h6{
     padding: 0 1rem;
+    text-align: center;
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    background-color: var(--c-txt-rev);
+    border-radius: var(--br-soft);
+    z-index: 1;
+    margin: 0;
+    border: solid 1px var(--c-txt);
+    transition: all .5s;
   }
 
   figure{
@@ -88,6 +125,8 @@ const StyledCard = styled.article`
     background-size: cover;
     margin: 0;
     transition: all .3s;
+    padding-bottom: 1rem;
+    /* filter: drop-shadow(1px 1px 3px white); */
 
     img{
       width: 250px;
@@ -99,23 +138,42 @@ const StyledCard = styled.article`
     flex: 1;
   }
 
+  .image-cont{
+    transition: all .3s;
+
+    &:hover, &:focus{
+      outline: none;
+      /* opacity: .7; */
+    }
+  }
+
   a.button{
     margin: 0 auto;
     margin-top: auto;
     max-width: 10rem;
-    padding: 0 2rem;
+    /* padding: 0 1rem; */
     text-align: center;
+    position: absolute;
+    bottom: 5px;
+
+    svg{
+      margin-left: .7rem;
+    }
   }
 
+
+
   &:hover{
-    border: solid 1px var(--c-accent) ;
+    /* border: solid 1px var(--c-accent) ; */
     border-radius: var(--br-dull);
 
-    h3{
+    h6{
       color: var(--c-accent);
+      top: -5px;
     }
     figure{
       background-position: 20% 80%;
+      transform: scale(1.03);
     }
   }
 `
