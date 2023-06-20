4d19f80468b2ed62774590b834d4c199765a9f6d
wchorski <wchorski@gmail.com>
2023-06-20 16:13:24 -0500

    block tweaks and footer creation

---
+++ frontend/components/menus/Footer.init.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link"
+import styled from "styled-components"
+
+
+export function Footer() {
+  return (
+    <StyledFooter>
+
+      <p>powered by <Link href={`https://www.tawtaw.site`}> {"There's a Will There's a Website"}</Link> </p>
+
+    </StyledFooter>
+  )
+}
+
+const StyledFooter = styled.footer`
+  /* background-color: green; */
+  max-width: var(--maxWidth);
+  display: flex;
+  justify-content: space-around;
+  margin: 0 auto;
+  position: relative;
+  color: var(--c-txt-rev);
+
+  /* &::before{
+    content: 'before';
+  } */
+  &::after{
+    content: '';
+    background-color: var(--c-dark);
+    /* background: 
+      linear-gradient(90deg, 
+        var(--c-bg) 0%, 
+        var(--c-3) 25%, 
+        var(--c-3) 75%, 
+        var(--c-bg) 100%); */
+    position: absolute;
+    z-index: -1;
+    inset: 0px;
+    /* transform: scaleX(3); */
+  }
+`
--- frontend/lib/copy-private-files.sh
+++ frontend/lib/copy-private-files.sh
@@ -4,6 +4,7 @@ echo "--- copying private files ---"
 
 cp ../components/menus/Nav.tsx ../private/components/menus/Nav.tsx
 cp ../components/Header.tsx ../private/components/Header.tsx 
+cp ../components/Footer.tsx ../private/components/Footer.tsx 
 cp ../components/elements/Layouts.tsx ../private/components/elements/Layouts.tsx
 cp -a ../components/private ../private/components/
 cp -a ../styles/private ../private/styles/private
