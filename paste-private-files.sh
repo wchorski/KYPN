1a0df367418a7a107adc66ba017feb7e7bb364bd
wchorski <wchorski@gmail.com>
2023-07-07 12:25:13 -0500

    better auto private copy paste scripts

--- paste-private-files.sh
+++ paste-private-files.sh
@@ -6,8 +6,8 @@ cp ./private/frontend/components/menus/Nav.tsx ./frontend/components/menus/Nav.t
 cp ./private/frontend/components/menus/Header.tsx ./frontend/components/menus/Header.tsx 
 cp ./private/frontend/components/menus/Footer.tsx ./frontend/components/menus/Footer.tsx 
 cp ./private/frontend/components/elements/Layouts.tsx ./frontend/components/elements/Layouts.tsx
-cp -a ./private/frontend/components/ ./frontend/components/private
-cp -a ./private/frontend/styles/private ./frontend/styles/private
+cp -a ./private/frontend/components/private ./frontend/components/
+cp -a ./private/frontend/styles/private ./frontend/styles/
 cp ./private/frontend/styles/GlobalThemeStyle.styled.js ./frontend/styles/GlobalThemeStyle.styled.js
 cp ./private/frontend/pages/home.tsx ./frontend/pages/home.tsx
 echo "--- pasting complete ---"
