2bc7b7d1940ec945b5f286e01f8f30246bdbc534
wchorski <wchorski@gmail.com>
2023-07-11 15:39:56 -0500

    first prisma migration

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -15,7 +15,7 @@ import { extendGraphqlSchema, lists } from './schema';
 
 // authentication is configured separately here too, but you might move this elsewhere
 // when you write your list-level access control functions, as they typically rely on session data
-import { withAuth, session } from './auth';
+import { withAuth, session,} from './auth';
 import { Context, TypeInfo } from '.keystone/types';
 
 const DATABASE_URL = process.env.DATABASE_URL || 'postgres://admin:admin@localhost:5432/keystone'
