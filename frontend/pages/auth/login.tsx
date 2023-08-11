b43d831c0f0f4808bdc99166ebbb2dca26d6460a
wchorski <wchorski@gmail.com>
2023-08-11 14:01:02 -0500

    loading animation actually interesting now

--- frontend/pages/auth/login.tsx
+++ frontend/pages/auth/login.tsx
@@ -9,8 +9,6 @@ export default function LoginPage() {
       <div className="container" >
         <StyledLoginWrap>
 
-          <QueryLoading />
-
           <LoginForm />
  
           <RegisterForm />
